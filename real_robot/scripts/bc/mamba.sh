a979a6340a965d5801c9069951bc4e2c26c71fba
Evil_Eyes <673269043@qq.com>
2026-03-10 18:56:24 +0800

    chore: ignore real_robot artifacts

--- real_robot/scripts/bc/mamba.sh
+++ real_robot/scripts/bc/mamba.sh
@@ -1,12 +1,12 @@
 python -m real_robot.train \
-  agents=ddim_agent \
-  agents/model=ddim/ddim_dec_transformer \
+  agents=bc_agent \
+  agents/model=bc/bc_dec_mamba \
   agents/obs_encoders=resnet \
   agents.if_film_condition=False \
   task_name=pick_up_the_orange_and_place_it_in_the_basket \
   wandb.mode=offline \
   scaler_type=standard \
-  epoch=50 \
+  epoch=100 \
   train_batch_size=64 \
   val_batch_size=64 \
-  num_workers=8
+  num_workers=16
